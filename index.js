e7bb112dedf5f55df1960a75a9a891923d28406a
rvaldez20 <rvaldez20@gmail.com>
2022-09-09 20:45:20 -0500

    [ADD] Se crea un servidor basico de node

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hola desde nodejs')
+import express from 'express';
+
+const app = express();
+
+app.use('/', (req, res) => {
+  res.send('Hola Mundo!!')
+})
+
+
+app.listen(4000, () => {
+  console.log('Server run on port 4000')
+})
