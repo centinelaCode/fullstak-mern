67badcc437e3bde42ec374dc98be1fa62814f70c
centinela1024tb <centinela1024tb@gmail.com>
2022-09-20 14:19:33 -0500

    [ADD] Se valida el token contra la base de datos
    => Si el token es valido: eliminamos el token=null y confirmado=true

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -22,9 +22,24 @@ const registro = async (req, res) => {
 }
 
 
-const confirmar = (req, res) => {
-  console.log(req.params.token)
-  res.json({msg: 'Confirmando cuenta...'})
+const confirmar = async(req, res) => {
+  const { token } = req.params;
+  const usuarioConfirmar = await Veterinario.findOne({token});
+  if(!usuarioConfirmar) {
+    const error = new Error('Token Ivalido');
+    return res.status(404).json({msg: error.message})
+  }
+
+  try {
+    usuarioConfirmar.token = null;
+    usuarioConfirmar.confirmado = true;
+    await usuarioConfirmar.save();
+
+    res.json({msg: 'Usuario confirmado Correctamente'});
+  } catch (error) {
+    console.log(error)
+  }
+
 }
 
 const perfil = (req, res) => {
