87da1e18890d0450c6e1f0b5825471d72905513b
centinela1024tb <centinela1024tb@gmail.com>
2022-09-28 13:14:45 -0500

    [ADD] Se configura el envio del email usando el transporte.sendMail
    => Se le pasa: from | to | subject | text | html
    => El html incluye el enlace hacia la url del frontend que permitira confirmar la cuenta

--- helpers/emailRegistro.js
+++ helpers/emailRegistro.js
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer';
 
-const emailRegistro = () => {
+const emailRegistro = async(data) => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
@@ -9,6 +9,25 @@ const emailRegistro = () => {
       pass: process.env.EMAIL_PASS,
     }
   });
+
+  const {nombre, email, token} = data;
+  
+  // TODO: Enviar el email
+  const info = await transporter.sendMail({
+    from: 'APV - Administrador de PAcientes de Veterinaria',
+    to: email,
+    subject: 'Comprueba tu cuenta en APV',
+    text: 'Comprueba tu cuenta en APV',
+    html: `<p>Hola ${nombre}, comprueba tu cuenta en APV.</p>
+           <p>Tu cuenta ya esta lista, solo debes comprobarla en el siguiente enlace:
+           <a href="${process.env.FRONTEND_URL}/confirmar/${token}">Comprobar Cuenta</a></p>
+
+           <p>Si tu no creaste una cuenta en APV puedes ignoirar este mensaje.</p>    
+    `,
+  });
+
+  console.log('Mensaje enviado: %s', info.messageId);
+
 }
 
 
